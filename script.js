de3b646e171eb02a8ef9e38b0e8ce579ede0ca41
vishvajeet shukla <djritikshukla@gmail.com>
2024-10-06 14:42:49 +0530

    add hire me (my contact )

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
+
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
-}
+};
 
-const typeed = new Typed(".text", {
-    strings: ["Frontend Developer"],
-    typeSpeed: 80,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true,
-})
-
- 
-
-document.getElementById('downloadBtn').addEventListener('click', function() {
-  const link = document.createElement('a');
-  link.href = './media/resume/vishu-resumeg.pdf'; 
-  link.download = 'vishvajeet-resume'; 
-  link.click(); 
+document.addEventListener("DOMContentLoaded", function() {
+    const typed = new Typed(".text", {
+        strings: ["Frontend Developer"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true,
+    });
 });
 
 
-let YearUpdate = document.getElementById('yearupdate')
-let date = new Date()
-let time =date.getFullYear()
-    YearUpdate.innerText = time
-    console.log(YearUpdate.innerText)
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = './media/resume/vishu-resume.pdf'; 
+    link.download = 'vishvajeet-resume.pdf'; 
+    link.click(); 
+});
+
+const yearUpdate = document.getElementById('yearupdate');
+yearUpdate.innerText = new Date().getFullYear();
 
 
 const phoneNumber = "+919782744729";
-
+const callButton = document.getElementById('callButton');
 callButton.setAttribute('href', `tel:${phoneNumber}`);
 
 
-callButton.addEventListener('click', function(event) {
-    console.log("Phone icon clicked! Initiating call to: " + phoneNumber);
+callButton.addEventListener('click', function() {
+   
 });
