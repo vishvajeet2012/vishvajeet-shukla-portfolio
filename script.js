afa1f4563ec030a68b25c8c8e6d6b92401f2c666
vishvajeet shukla <djritikshukla@gmail.com>
2024-10-21 12:00:06 +0530

    update package.json fromat java script

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    const typed = new Typed(".text", {
-        strings: ["Frontend Developer"],
-        typeSpeed: 80,
-        backSpeed: 50,
-        backDelay: 1000,
-        loop: true,
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = new Typed(".text", {
+    strings: ["Frontend Developer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+  });
 });
 
-
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = './media/resume/vishu-resume.pdf'; 
-    link.download = 'vishvajeet-resume.pdf'; 
-    link.click(); 
+document.getElementById("downloadBtn").addEventListener("click", function () {
+  const link = document.createElement("a");
+  link.href = "./media/resume/vishu-resume.pdf";
+  link.download = "vishvajeet-resume.pdf";
+  link.click();
 });
 
-const yearUpdate = document.getElementById('yearupdate');
+const yearUpdate = document.getElementById("yearupdate");
 yearUpdate.innerText = new Date().getFullYear();
 
-
-
-
 function phoneClick() {
-// is code me kuch changes mt krna nahi to number browser main  hi open hoga
-// const myarray = [9, 2, 7, 4, 4, 7];
+  // is code me kuch changes mt krna nahi to number browser main  hi open hoga
+  // const myarray = [9, 2, 7, 4, 4, 7];
 
-// myarray.push(2, 8);
-// myarray.push(7, 9);
+  // myarray.push(2, 8);
+  // myarray.push(7, 9);
 
-// const a = myarray.reverse();
+  // const a = myarray.reverse();
 
-// const v = a.toString();
+  // const v = a.toString();
 
+  // console.log(v);
 
-// console.log(v);
+  const vishuNumber = "9782744729";
 
-const vishuNumber = "9782744729";
+  const callButton = document.getElementById("callButton");
+  callButton.setAttribute("href", `tel:${vishuNumber}`);
 
+  // callButton.addEventListener('click', function() {
 
-const callButton = document.getElementById('callButton');
-callButton.setAttribute('href', `tel:${vishuNumber}`);
-
-// callButton.addEventListener('click', function() {
-   
-// });
-
+  // });
 }
